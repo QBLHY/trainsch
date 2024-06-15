4c003688b78c39fb9be6d60ea7fafa897f111c06
YirongWho <huyirong97@gmail.com>
2024-06-15 15:34:14 +0800

    fix bugs in AL method

--- ALM.py
+++ ALM.py
@@ -1,11 +1,10 @@
 from prepare import model_param
 from algorithms import ALM,vec_to_path,path_to_vec
 import json
-
+ 
 param=model_param()
-x,val=ALM(param,num_train=16,k_max=1,t_max=10,mu=0.1,alpha=1)
+x,val=ALM(param,num_train=16,k_max=8,t_max=5,mu=10,alpha=1)
 path=vec_to_path(x)
-
 for i in range(1,17):
     path[i].insert(0,('S',0))
     path[i].pop() # remove the last station
@@ -13,3 +12,5 @@ for i in range(1,17):
 # save path
 with open('data/alm_solution.json', 'w') as f:
     json.dump(path, f)
+
+print('the obj val is', val)
--- algorithms.py
+++ algorithms.py
@@ -1,13 +1,13 @@
 import copy
 from tqdm import tqdm
 import time 
+from prepare import conflict_node
 
 ###########################################
 ### 这个文件夹中包含了一些需要被调用的算法
 ###########################################
 def idx(i):
     return str(i) if i in range(1,7) else '0'
-
 
 
 '''
@@ -49,10 +49,10 @@ INPUT
 OUTPUT
     x: a dictionary, key is the train NO, value is a dictionary.
 '''
-def path_to_vec(path):
-    x={}
+def path_to_vec(path,param):
+    x={j:{arc:0 for arc in param[idx(j)]['arcs']}   for j in path.keys()}
     for train in path.keys():
-        x[train]={}
+        if len(path[train])==0 : continue
         x[train][(('S',0),tuple(path[train][0]))]=1
         for _ in range(len(path[train])-1):
             x[train][(path[train][_],path[train][_+1])]=1
@@ -85,71 +85,69 @@ num_train: 默认为16
 OUTPUT: 成功则返回一个可行解fea_sol, 否则返回NONE. fea_sol也是一些路径
 '''
 def find_feasible(param,mul,sol,num_train=16):
-    fea_sol={train:[] for train in range(1,num_train+1)}
+    fea_sol=copy.deepcopy(sol)
     # compute dual obj function for each train
-    dual_obj = {train:0 for j in range(1,num_train+1)}
+    dual_obj = {train:0 for train in range(1,num_train+1)}
     pe=param['pe']
     # train 为列车编号
     for train in range(1,num_train+1): 
-        path = sol[str(train)]
+        path = sol[train]
         # 获得对应节点和边
-        Ej = param[idx(train)]['arcs']
         for _ in range(len(path)-1):
             arc= tuple([tuple(path[_]),tuple(path[_+1])])
             t=arc[1] # arc=(*,t)
             # 计算这条边arc在dual obj function中前面对应的系数
-            coeff=pe[str(arc)]
+            coeff=pe[arc]
             if t[0]!='T':
                 neighbors=param['N'][str(t)]
                 for v in neighbors:
-                    coeff-=mul[str(v)]
+                    coeff-=mul[v]
             dual_obj[train]+=coeff
     
     # 对dual_obj 进行排序
-    sorted_dual = sorted(dual_obj.items(), key=lambda x: x[1],reverse=False)
+    sorted_dual = sorted(dual_obj.items(), key=lambda x: x[1],reverse=False) 
     priority_list=[item[0] for item in sorted_dual]
 
     # 逐个替换列车
-    done=set() # 已经安排好的列车集合
-    for _ in range(5): # 最多执行5轮
+    for _ in range(1): # 最多执行1轮
         for train in priority_list:
-            if train in done:
-                continue
             # 获得对应节点和边
-            Ej = copy.deepcopy(param[idx(train)]['arcs'])
-            Vj = copy.deepcopy(param[idx(train)]['nodes'])
+            E_j = set(param[idx(train)]['arcs'])
+            V_j = set(param[idx(train)]['nodes'])
             # 去掉与其他列车冲突的node以及arc
             for t in range(1,num_train+1):
                 if t!=train:
-                    path = sol[str(t)] # the path of the train t
+                    path = fea_sol[t] # the path of the train t of the current fea_sol
                     if (len(path)==0): continue
                     # nodes on this path
                     nodes={tuple(path[_]) for _ in range(len(path)-1)}
-                    conflict_nodes = {v_ for v in nodes for v_ in param['N'][str(v)]}
-                    conflict_arcs  = {arc for arc in Ej if arc[1] in conflict_nodes}
+                    # 注意，这里计算conflict_nodes 需要使用的半径是4，而不是2!!
+                    conflict_nodes = {v_ for v in nodes for v_ in conflict_node(v,4)} 
                     # remove nodes from Vj
-                    Vj = set(Vj) - conflict_nodes
+                    V_j = V_j - conflict_nodes
+                    conflict_arcs  = {arc for arc in E_j if (arc[1] in conflict_nodes or arc[0] in conflict_nodes)}
                     # remove arcs from Ej
-                    Ej = set(Ej) - conflict_arcs
-                    # 使用Bellman-Ford算法计算新的路径
-            dist,new_path=BF(Vj,Ej,pe,tuple(path[0]),tuple(path[-1]))
-            # 如果新的路径不为空，则更新fea_sol
-            if new_path:
-                fea_sol[train]=new_path
-                done.add(train)
-    if len(done)==16:   return fea_sol
-    else:               return None
-        
+                    E_j = E_j - conflict_arcs
+            # 使用Bellman-Ford算法计算新的路径
+            dist,new_path=BF(V_j,E_j,pe,('S',0),('T',0)) 
+            # 更新fea_sol
+            fea_sol[train]=new_path
+    return fea_sol
+
+
 '''
 x is a dictionary, x[j] is a dictionary, x[j][arc] is the value of x_j(arc)
 Ej is a dictionary, Ej[j] is the set of arcs of train j
 V is the set of nodes
 N is the dictionary, N[v] is the set of neighbors of node v
+ignore is a set (default empty), ignore the train in this set.
 '''
-def compute_y_and_Y(x, Ej, num_train, V, N):
+def compute_y_and_Y(x, Ej, num_train, V, N,ignore=set()):
     # Compute y_v
     y = {v: 0 for v in V}
     for _ in range(1, num_train + 1):
+        if _ in ignore:
+            continue
         for arc in Ej[_]:
             if x[_][arc] == 0: continue
             if (to := arc[1]) != ('T', 0): y[to] += 1
@@ -164,7 +162,7 @@ def compute_y_and_Y(x, Ej, num_train, V, N):
 customized augumented Lagrangian method
 '''
 
-def ALM(param,num_train=16,k_max=10,t_max=1,mu=0.1,alpha=1):
+def ALM(param,num_train=16,k_max=10,t_max=1,mu=10,alpha=10):
     # 读取参数
     pe=param['pe']
     N=param['N']
@@ -181,59 +179,52 @@ def ALM(param,num_train=16,k_max=10,t_max=1,mu=0.1,alpha=1):
     objval=float('-inf')
     # 初始化最初的x
     x={j:{arc:0 for arc in Ej[j]} for j in range(1,num_train+1)} # 全0初始化
-    
-    BF_time =0 # TODO: remove this line
-    update_time=0
-    BCD_time=0
-    Y_time=0
-    grad_time=0
-    grad1_time=0
+    x_={}
     # 开始外层循环
     for k in tqdm(range(k_max)):
         # 开始内层循环
         for t in range(t_max):
             for j in range(1,num_train+1): # j is the NO of train
                 # 为了更新x_j, 我们必须算出此时增广拉格朗日函数对x_j的梯度
-                # 先计算y_v,Y_v
-                y,Y=compute_y_and_Y(x,Ej,num_train,V,N)
-                # 下面计算梯度, 首先考虑来自原始目标函数的部分
-                grad={arc:pe[str(arc)] for arc in Ej[j]}
+                # 先计算y_v,Y_v , 忽略第j个train
+                y,Y=compute_y_and_Y(x,Ej,num_train,V,N,ignore={j})
+                # 下面计算BCD迭代需要的权重, 首先考虑来自原始目标函数的部分
+                weights={arc:pe[arc] for arc in Ej[j]}
                 # 下面考虑来自对偶目标函数的部分
                 for arc in Ej[j]:
                     if (to := arc[1]) != ('T', 0):  # 如果是指向虚拟终点的边，这些边不会出现在对偶目标函数中
-                        grad[arc] -= sum(lmd[v] + rho * max(0, Y[v]) for v in N[str(to)])
-                # 下面计算需要传入给BF算法的weight
-                weights={arc: mu*grad[arc]-1/2+x[j][arc] for arc in Ej[j]}
+                        #grad[arc] -= sum(lmd[v] + rho * max(0, Y[v]-1) for v in N[str(to)]) proximal linear 的方法，效果不好
+                        weights[arc] -= sum(lmd[v]+rho*max(0,Y[v]-1/2)  for v in N[str(to)])
+                
 
                 new_path=BF(V_,Ej[j],weights,('S',0),('T',0))[1]
-                
-                
                 if len(new_path)==0 : print('error occurs at BF in BCD') # just for debug
                 # 开始更新x_j
                 x[j]={arc:0 for arc in Ej[j]}
                 x[j][(('S',0),(new_path[0]))]=1
                 for _ in range(1,len(new_path)):
                     x[j][(new_path[_-1],new_path[_])]=1
-        # 检查BCD得到的解是否可行
         
-        if max(Y.values())>1: # BCD的解不可行
+        #检查BCD得到的解是否可行   
+        y,Y=compute_y_and_Y(x,Ej,num_train,V,N)
+        if max(Y.values())>1 : # BCD的解不可行
+            print('BCD not feasible, use heuristic!')
             new_sol=find_feasible(param,lmd,vec_to_path(x),num_train)
-            if new_sol is None:
-                print('error occurs at heuristic algorithm in ALM')
-                return None
-            x= path_to_vec(new_sol)  
+            x_= path_to_vec(new_sol,param)  
+        else: 
+            x_=copy.deepcopy(x) 
+        # 更新目标函数值 
         new_objval=0
         for train in range(1,num_train+1):
-            new_objval+=sum(pe[str(arc)]*x[train][arc] for arc in Ej[train])
+            new_objval+=sum(pe[arc]*x_[train][arc] for arc in Ej[train])
         if new_objval>objval:
             objval=new_objval
-            x_star=x
+            x_star=x_
         # update multipliers and penalty coefficients
-        y,Y=compute_y_and_Y(x,Ej,num_train,V,N)
+        print('objval:',objval)
         for _ in lmd.keys():
             lmd[_]=max(0,lmd[_]+alpha*(Y[_]-1))
         rho+=alpha/2*sum(max(0,value-1)**2 for value in Y.values())
-    
     return x_star,objval
 
 
