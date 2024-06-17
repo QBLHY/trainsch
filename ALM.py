a9819aa04ee94e929d5bbc6b7765ac80595d118a
YirongWho <huyirong97@gmail.com>
2024-06-17 19:34:51 +0800

    add max_iter

--- ALM.py
+++ ALM.py
@@ -3,9 +3,9 @@ from algorithms import ALM,vec_to_path,path_to_vec
 import json
 import argparse
 
-def main(setting):
+def main(setting,max_iter):
     param=model_param(setting=setting)
-    x,val=ALM(param,num_train=16,k_max=70,t_max=1,alpha=1)
+    x,val=ALM(param,num_train=16,k_max=max_iter,t_max=1,alpha=1)
     path=vec_to_path(x)
     for i in range(1,17):
         path[i].insert(0,('S',0))
@@ -20,5 +20,6 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--setting', type=int, default=2)
+    parser.add_argument('--max_iter', type=int, default=70)
     args = parser.parse_args()
-    main(args.setting)
+    main(args.setting,args.max_iter)
